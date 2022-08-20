02da19fa5e4c692fcdc8e60d4ebd4ac6b2dadefc
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-08-20 16:52:07 +0530

    Add A1_Q1 tests.

--- Assignment1/A1Q1_top.v
+++ Assignment1/A1Q1_top.v
@@ -15,17 +15,26 @@ module eight_bit_adder_subtractor_top;
 
     initial
     begin
-        $monitor("<%d>: Data0 = %d, Data1 = %d, mode = %b, final_sum = %d, final_carry_out = %b, overflow = %b\n", $time, Data0, Data1, mode, final_sum, final_carry_out, overflow);
+        $monitor("<%2d>: Data0 = %4d, Data1 = %4d, mode = %b, final_sum = %4d, final_carry_out = %b, overflow = %b\n", $time, Data0, Data1, mode, final_sum, final_carry_out, overflow);
     end
 
     initial
     begin
         Data0 = 2; Data1 = 1; mode = 1;
         #5 Data0 = 2; Data1 = 1; mode = 0;
-        #10 Data0 = 15; Data1 = 10; mode = 1; 
-        #15 Data0 = 15; Data1 = 10; mode = 0; 
-        #20 Data0 = 10; Data1 = 15; mode = 0; 
-        #25 Data0 = -10; Data1 = 15; mode = 0; 
+        #5 Data0 = 15; Data1 = 10; mode = 1; 
+        #5 Data0 = 15; Data1 = 10; mode = 0; 
+        #5 Data0 = 10; Data1 = 15; mode = 0; 
+        #5 Data0 = -10; Data1 = 15; mode = 0; 
+        #5 Data0 = 127; Data1 = 129; mode = 1;
+        #5 Data0 = 127; Data1 = 127; mode = 1;
+        #5 Data0 = 128; Data1 = 128; mode = 1;
+        #5 Data0 = 128; Data1 = 128; mode = 0;
+        #5 Data0 = -128; Data1 = -128; mode = 1;
+        #5 Data0 = 101; Data1 = -101; mode = 0;
+        #5 Data0 = 63; Data1 = 65; mode = 1;
+        #5 Data0 = 63; Data1 = 64; mode = 1;
+        #5 Data0 = 126; Data1 = 2; mode = 1;
     end
 
 endmodule
